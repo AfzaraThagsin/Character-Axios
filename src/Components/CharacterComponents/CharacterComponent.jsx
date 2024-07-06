f93e3a3de4297b086bf44d582908ccd86a9ea89a
AFZARATHAGSIN <afzarashajahan@gmail.com>
2024-07-06 14:07:23 +0530

    first commit

--- src/Components/CharacterComponents/CharacterComponent.jsx
+++ src/Components/CharacterComponents/CharacterComponent.jsx
@@ -1,44 +1,16 @@
-// import React, { useState } from 'react'
-//  import './CharacterComponent.css'
 
-// const CharacterComponent = () => {
-//     const [char,setChars]=useState([])
-
-//     const getDataHandler=async()=>
-//     {
-//         const user = await axios.get('https://rickandmortyapi.com/api/character');        setChars(user.data)
-        
-//     }
-//   return (
-//     <React.Fragment>
-//     <button onClick={getDataHandler}>Get Data</button>
-//     <div className='users'>
-//         {char && char.map((iterator,index)=>
-//             (
-//                 <div className='user' key={index}> to keep on iterate this we isolate this in separate component as user component using props is same as the parameter in function
-//                 <p>{iterator.name}</p>
-//                 <p>{iterator.email}</p>
-//                 <p>{iterator.website}</p>
-//                 </div>
-//             )
-//         )}
-//     </div>
-//     </React.Fragment>
-//   )
-// }
-
-// export default CharacterComponent
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './CharacterComponent.css';
 
 const CharacterComponent = () => {
   const [chars, setChars] = useState([])
+ const [pageNumber,setPageNumber]=useState(1)
 
   useEffect(() => {
     const getDataHandler = async () => {
       try {
-        const user = await axios.get('https://rickandmortyapi.com/api/character');
+        const user = await axios.get(`https://rickandmortyapi.com/api/character?page=${pageNumber}`);
         setChars(user.data.results);
       } catch (error) {
         console.error("Error fetching data: ", error);
@@ -46,7 +18,15 @@ const CharacterComponent = () => {
     };
 
     getDataHandler();
-  }, []);
+  }, [pageNumber]);
+  
+  const prevHandler = () => {
+    setPageNumber(pageNumber-1)
+  }
+
+  const nextHandler = () => {
+    setPageNumber(pageNumber+1)
+  }
 
   return (
     <React.Fragment>
@@ -55,21 +35,26 @@ const CharacterComponent = () => {
         
         
         {chars && chars.map((iterator) => (
-            <div className='app' >
-                <img  className='show'src={iterator.image} alt={iterator.name}>
-                </img>
-          <div className='user' key={iterator.id}>
-            <p>{iterator.name}</p>
-            <p>{iterator.status}</p>
-            <p>{iterator.species}</p>
+          <div className="user-ele">
+              <div className='app' >
+                  <img  className='show'src={iterator.image} alt={iterator.name}></img>
+              </div>
+              <div className='user' key={iterator.id}>
+                <p>{iterator.name}</p>
+                <p>{iterator.status}</p>
+                <p>{iterator.species}</p>
+              </div>
+              
           </div>
-             </div>
          
        
          
 
         ))}
       </div>
+      <button onClick={prevHandler}>Prev</button>
+      <p>{pageNumber}</p>
+      <button onClick={nextHandler}>Next</button> 
     </React.Fragment>
   );
 }
